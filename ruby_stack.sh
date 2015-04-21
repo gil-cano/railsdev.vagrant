a82deee8ef5cde3225013375eec42a13b797c74b
gil-cano <gil.cano@gmail.com>
2015-04-21 13:31:29 -0500

    add rails

--- ruby_stack.sh
+++ ruby_stack.sh
@@ -1,8 +1,9 @@
  #!/usr/bin/env bash
 
 apt-get install curl
-#curl -#LO https://rvm.io/mpapis.asc
-#gpg --import mpapis.asc
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.2
-apt-get install nodejs
+apt-get -y install nodejs
+#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+#curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.2
+source /usr/local/rvm/scripts/rvm
+rvm use ruby-2.2.2@rails4.2.1 --create
+gem install rails
