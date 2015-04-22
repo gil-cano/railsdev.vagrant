10ed8d607d68ccc1376fa9748ff51110bd45965b
gil-cano <gil.cano@gmail.com>
2015-04-21 20:40:50 -0500

    pin ruby version

--- ruby_stack.sh
+++ ruby_stack.sh
@@ -6,4 +6,4 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.2
 source /usr/local/rvm/scripts/rvm
 rvm use ruby-2.2.2@rails4.2.1 --create
-gem install rails
+gem install rails --version=4.2.1
